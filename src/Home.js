b75a6bd03745a72b83bacc850b33b8c942e95e16
pavansahith <cs19b017@iittp.ac.in>
2021-10-18 23:01:41 +0530

    homepage updated

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,58 @@
 import React from 'react'
-
+import logo from './logo.svg';
+import './Home.css'
 export default function Home() {
   return (
-  <>
-  <h1>This is home page</h1>
-  <h1>pk</h1>
-  <h1>yethin</h1>
-  
-  </>
+    <>
+      <h1>This is home page</h1>
+      <button type="button" className="btn btn-dark" >Dark</button>
+
+      <div className="container">
+        <img src={logo} className="img-1" alt="..."></img>
+        <button type="button" className="btn btn-success">Success</button>
+      </div>
+      <div className="row">
+
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+        <div class="col-lg-2 col-md-4">  <div className="container">
+          <img src={logo} className="img-1" alt="..."></img>
+          <button type="button" className="btn btn-success">Success</button>
+        </div></div>
+
+        {/* <img src={logo} className="img-1" alt="..."></img>
+        <img src={logo} className="img-1" alt="..."></img>
+        <img src={logo} className="img-1" alt="..."></img>
+        <img src={logo} className="img-1" alt="..."></img>
+        <img src={logo} className="img-1" alt="..."></img> */}
+
+
+      </div>
+      <div>
+
+      </div>
+      {/* <div class="row">
+       
+      </div> */}
+
+    </>
   )
 }
