dadf770babaa8e5a626e0472cb0f46b622ac461a
Jaswanth <cs19b042@iittp.ac.in>
2021-10-25 14:56:43 +0530

    main page updated

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import Main from './Main';
-// import Navbar from '../components/Navbar';
 
 const Login = () => {
     return(
         <div>
-            {/* <Main/> */}
         <h1>This is Login</h1>
         <Link to="/main" className="btn btn-primary">Go to</Link>
         </div>  
