efe14abb2654d2fcea1ff81fbf44eec582e7fabe
pavansahith <cs19b017@iittp.ac.in>
2021-10-30 22:34:07 +0530

    inventory page

--- Frontend/src/Addproduct.js
+++ Frontend/src/Addproduct.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 // Which adds the house hold products to inventory
 export default function Addproduct() {
+    const [item, setitem] = useState({ItemName:"",storage:""})
+    const additem=()=>{
+        var name=document.getElementById("Textarea1").value
+        // var storage=document.getElementById("Textarea1").value
+        setitem({ItemName:name,storage:"almirah"})
+        console.log(item)
+
+    }
     return (
         <div>
             <h1>This add product page</h1>
             <div className="mb-3 row">
                 <label htmlFor="inputtext" className="col-sm-2 col-form-label">Item Name</label>
                 <div className="col-sm-2">
-                    <textarea className="form-control" id="exampleFormControlTextarea1" rows="1"></textarea>
+                    <textarea className="form-control" id="Textarea1" rows="1"></textarea>
                 </div>
             </div>
             <select >
@@ -17,7 +25,7 @@ export default function Addproduct() {
                 <option value="3">other</option>
             </select>
            <br/>
-            <a class="btn btn-success" href="#" role="button">Add Product</a>
+            <a class="btn btn-success" href="#" role="button" onClick={additem}>Add Product</a>
         </div>
     )
 }
---
+++ Frontend/src/components/Inventory.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Inventory() {
+    return (
+        <div>
+            <input type="file"></input>
+        </div>
+    )
+}
+
+
