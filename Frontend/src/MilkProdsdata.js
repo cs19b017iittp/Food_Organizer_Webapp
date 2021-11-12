728a0a61e8844bdb783e5122e50f0b26a74ddf41
pavansahith <cs19b017@iittp.ac.in>
2021-11-12 22:26:55 +0530

    home page data

---
+++ Frontend/src/MilkProdsdata.js
@@ -0,0 +1,36 @@
+const data ={
+    cardData:[
+        {
+            id:1,
+            img: 'https://cdn.pixabay.com/photo/2016/12/06/18/27/milk-1887234__340.jpg',
+            title: 'Milk',
+        },
+        {
+            id:2,
+            img: 'https://cdn.pixabay.com/photo/2018/09/03/20/06/cheese-3652274__480.jpg',
+            title: 'Cheese',
+        },
+        {
+            id:3,
+            img: 'https://cdn.pixabay.com/photo/2013/07/12/19/04/popsicle-154321__480.png',
+            title: 'IceCream',
+        },
+        {
+            id:4,
+            img: 'https://cdn.pixabay.com/photo/2016/06/11/04/09/butter-1449453__340.jpg',
+            title: 'Butter',
+        },
+        {
+            id:5,
+            img: 'https://media.istockphoto.com/photos/ghee-or-clarified-butter-picture-id658829932?b=1&k=20&m=658829932&s=170667a&w=0&h=7wlzsgbzu9IpqfD_taPr5wCy2jH_-BeVKw-EwxVyWhI=',
+            title: 'Ghee',
+        },
+        {
+            id:6,
+            img: 'https://media.istockphoto.com/photos/sour-cream-or-yogurt-in-wooden-bowl-picture-id1135483716?b=1&k=20&m=1135483716&s=170667a&w=0&h=rA6ZUwTvVCrpVh8-m69US7kNC4PU5I_8p93Owzy4drc=',
+            title: 'Yogurt',
+        },
+    ]
+}
+
+export default data;
