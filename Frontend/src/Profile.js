ce9d8763c1502c36048b40d118abb986257d2b40
mycs <yethinchandra2635@gmail.com>
2021-11-18 22:11:06 +0530

    dark mode

--- Frontend/src/Profile.js
+++ Frontend/src/Profile.js
@@ -1,15 +1,34 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import ReactNotification from 'react-notifications-component';
+import { store } from 'react-notifications-component';
+import 'react-notifications-component/dist/theme.css';
+import 'animate.css';
+import "./Boxes.css";
+//import DatePicker from 'react-date-picker';
+import { MdHeight } from 'react-icons/md';
+//import Datepick from './Datepick';
+import { utils } from 'react-modern-calendar-datepicker';
+import 'react-modern-calendar-datepicker/lib/DatePicker.css';
+import DatePicker from 'react-modern-calendar-datepicker';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+//
+// import React, { useState } from 'react'
+// import Footer from './components/Footer/Footer'
+// import Navbar from './components/Navbar'
+// import './Profile.css'
+// import axios from 'axios';
 
-import React from 'react'
-import Footer from './components/Footer/Footer'
-import Navbar from './components/Navbar'
-import './Profile.css'
 
 //Profile page shows user details
 export default function Profile() {
+   // const [email, setemail] = useState("")
 
-    // const setData = ()=>{
-
-    // }
+   // var x = localStorage.getItem("userName");
+   // setemail(x)
+   // console.log(x +"    profile page");
+   //users.find( { emailId: localStorage.getItem("userName") } );
     return (
         <>
             <Navbar />
@@ -24,7 +43,7 @@ export default function Profile() {
                 <div className="mb-3 row">
                     <label htmlFor="inputemail" className="col-sm-2 col-form-label">Email</label>
                     <div className="col-sm-10">
-                    <textarea className="form-control" id="exampleFormControlTextarea2" rows="1"></textarea>
+                    <textarea className="form-control" id="fillemail" rows="1" value={localStorage.getItem("userName")}></textarea>
                     </div>
                 </div>
                 <div className="mb-3 row">
