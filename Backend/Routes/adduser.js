b58be98f3cdeff7192bc658d0fe375cfca25c44a
pavansahith <cs19b017@iittp.ac.in>
2021-11-22 20:18:52 +0530

    authentication

--- Backend/Routes/adduser.js
+++ Backend/Routes/adduser.js
@@ -1,45 +1,67 @@
-const express=require("express");
-const router=express.Router();
-const userdetails=require("../Models/User");
+const express = require("express");
+const router = express.Router();
+const userdetails = require("../Models/User");
 const { where, $where } = require("../Models/User");
 
-var mail
+var mail;
 
-router.post("/insert",async(req,res)=>{
-    const name=req.body.username
-    const mail=req.body.emailId
-    const phoneNum = req.body.phone
-    const gen = req.body.gender
-    const password=req.body.password
- const user=new userdetails({username:name,emailId:mail,phone:phoneNum,gender:gen,password:password});
- try{
+router.post("/insert", async (req, res) => {
+  const name = req.body.username;
+  const mail = req.body.emailId;
+  const phoneNum = req.body.phone;
+  const gen = req.body.gender;
+  const password = req.body.password;
+  const user = new userdetails({
+    username: name,
+    emailId: mail,
+    phone: phoneNum,
+    gender: gen,
+    password: password,
+  });
+  try {
+    await user.save();
+    res.send("inserted data");
 
- await user.save();
- res.send("inserted data")
-
-
- console.log(user)
- }catch(err){
+    console.log(user);
+  } catch (err) {
     console.log(err);
- }
+  }
 });
 
- router.post("/getmailid",async(req,res)=>{
-    mail=req.body.emailId
-    console.log(mail)
+router.post("/getmailid", async (req, res) => {
+  mail = req.body.emailId;
+  console.log(mail);
+  //   console.log(mail);
+  //   res.send(mail);
 });
 
 router.get("/read", async (req, res) => {
-// console.log(mail)
- userdetails.find({},(err,result)=>{
-    if(err)
-    {
-       res.send(err)
+  // console.log(mail)
+  userdetails.find({ mail }, (err, result) => {
+    if (err) {
+      res.send(err);
     }
     res.send(result);
-   //  console.log(result)
+    //  console.log(result)
+  });
+});
 
- }) 
-   
- });
-module.exports=router 
+// //login route
+
+// router.post("/signin", async (req, res) => {
+//   try {
+//     const { emailId, password } = req.body;
+//     if (!emailId || !password) {
+//       return res.status(400).json({ error: "please fill the details" });
+//     }
+
+//     const userLogin = await user.findOne({ email: emailId });
+
+//     if (!userLogin) {
+//       res.json({ error: "user not der " });
+//     } else {
+//       res.json({ message: "user signin sucessfull" });
+//     }
+//   } catch (err) {}
+// });
+module.exports = router;
--- Backend/index.js
+++ Backend/index.js
@@ -1,36 +1,38 @@
-const express =require("express");
-const mongoose =require("mongoose");
-const app=express();
-const cors=require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const cors = require("cors");
 app.use(express.json());
 app.use(cors());
-const DB="mongodb+srv://foodorg:foodorg@userdetails.7jjbg.mongodb.net/users?retryWrites=true&w=majority";
+const DB =
+  "mongodb+srv://foodorg:foodorg@userdetails.7jjbg.mongodb.net/users?retryWrites=true&w=majority";
 
-app.use("/user",require("./Routes/adduser"))
-app.use("/item",require("./Routes/additems"))
-app.use("/userspecific",require("./Routes/readuser"))
+app.use("/user", require("./Routes/adduser"));
+app.use("/item", require("./Routes/additems"));
+app.use("/login", require("./Routes/getuser"));
+app.use("/userspecific", require("./Routes/readuser"));
 
-mongoose.connect(DB,{
-useNewUrlParser:true,
-}).then(()=>{
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then(() => {
     console.log("connection succesfull");
-}).catch((err)=>console.log(`no connectionn`));
-
-
+  })
+  .catch((err) => console.log(`no connectionn`));
 
 // app.get("/",async(req,res)=>{
-   
+
 //  const user=new userdetails({username:"yethin",emailId:"cs19b026@iittp.ac.in",password:1234567});
 //  try{
 //  await user.save();
 //  res.send("inserted data")
-
 
 //  console.log(user)
 //  }catch(err){
 //     console.log(err);
 //  }
 // });
-app.listen(3001,()=>{
-    console.log("server is set")
+app.listen(3001, () => {
+  console.log("server is set");
 });
